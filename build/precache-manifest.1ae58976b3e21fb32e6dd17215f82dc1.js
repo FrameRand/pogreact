048858afad6ec0ca9f728b1badc41a938f1e8e13
FrameRand <62851087+FrameRand@users.noreply.github.com>
2020-08-03 17:34:10 +0300

    Add files via upload

---
+++ build/precache-manifest.1ae58976b3e21fb32e6dd17215f82dc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60fd60494716d4bdc6ffecd82f7bb4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57b89632c202b5d1352e",
+    "url": "/static/css/main.20f5e7e0.chunk.css"
+  },
+  {
+    "revision": "d5f68d15cccd14ae4ad1",
+    "url": "/static/js/2.c5c67f2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c5c67f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57b89632c202b5d1352e",
+    "url": "/static/js/main.03757fcd.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
